81bbb2b800e7ca2ee8c6964137b5cea01aeae6af
7sena <40254036+7sena@users.noreply.github.com>
2019-02-22 15:49:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,29 @@ client.on('ready', function(){
 });
 
 client.on('message', message => {
+    if (message.content.startsWith("$bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Super Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+			      .addField('``My Prefix``' , `[ - ]` , true)
+			      .addField('``My Language``' , `[ Java Script ]` , true)
+			      .setFooter('By | KillerFox ')
+    })
+}
+});
+
+
+client.on('message', message => {
         if (message.content === "$inv") {
             if(!message.channel.guild) return;
         let embed = new Discord.RichEmbed()
